40a697c84eddf1e8e3b8432baeb42a89c44e904b
hhzule <hhzule@gmail.com>
2023-04-09 08:50:57 +0500

    markers changed

--- main.js
+++ main.js
@@ -6,22 +6,48 @@ var baseMap = {
   "World Imagery": imageryMap
 }
 
-
-// var geoJ = L.geoJSON("./resources/Layers/Barriers.geojson").addTo(map)
-
 var map = L.map('map',{
 center: [35.966436, -84.17407],
 zoom: 13,
 layers : [osmMap]
 })
 
-var lyrBarrier = L.geoJson.ajax('resources/Layers/Barriers.geojson').addTo(map)
-var lyrBuffer = L.geoJson.ajax('resources/Layers/Buffer_3000Ft.geojson').addTo(map)
-var lyrBuildings = L.geoJson.ajax('resources/Layers/Buildings.geojson').addTo(map)
-var lyrFiberRoutes = L.geoJson.ajax('resources/Layers/FiberRoutes.geojson').addTo(map)
+var lyrBarrier = L.geoJson.ajax('resources/Layers/Barriers.geojson',{
+  style: function (feature) {
+    return {color: "green"};
+    }
+}).addTo(map)
+var lyrBuffer = L.geoJson.ajax('resources/Layers/Buffer_3000Ft.geojson',
+{
+  style: function (feature) {
+    return {color: "red"};
+    }
+}).addTo(map)
+var lyrBuildings = L.geoJson.ajax('resources/Layers/Buildings.geojson',
+{
+  pointToLayer: function (geoJsonPoint, latlng) {
+    return L.circleMarker(latlng,{
+      color:"purple"
+    });
+    }
+}).addTo(map)
+var lyrFiberRoutes = L.geoJson.ajax('resources/Layers/FiberRoutes.geojson',
+{
+  style: function (feature) {
+    return {color: "black"};
+    }
+}).addTo(map)
 var lyrIntersecting = L.geoJson.ajax('resources/Layers/ROW_Paths_Intersecting_Barriers.geojson').addTo(map)
 var lyrNonIntersecting = L.geoJson.ajax('resources/Layers/ROW_Paths_Not_Intersecting_Barriers.geojson').addTo(map)
-var lyrSplice = L.geoJson.ajax('resources/Layers/Splice_Points.geojson').addTo(map)
+var lyrSplice = L.geoJson.ajax('resources/Layers/Splice_Points.geojson',
+{
+  pointToLayer: function (geoJsonPoint, latlng) {
+    return L.circleMarker(latlng,{
+      radius: 7,
+      color:"brown"
+    });
+    }
+}).addTo(map)
 
 var ObjOverLays = {
   "Barrier": lyrBarrier,
@@ -36,8 +62,8 @@ var mapLayers = L.control.layers(baseMap,
   ObjOverLays
   ).addTo(map)
 
-// var ctlMeasure = L.control.polylineMeasure({
-//   position: "topleft",
-//   measureControlTitle: "Measure Length"
-// }).addTo(map)
+var ctlMeasure = L.control.polylineMeasure({
+  position: "topleft",
+  measureControlTitle: "Measure Length"
+}).addTo(map)
 
